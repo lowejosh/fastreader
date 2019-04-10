4899efe1e79db20fc14de1841f5aa63b5373fbd5
Joshua Lowe <lowejosh50@gmail.com>
2019-04-10 15:18:23 +1000

    changing the structure

--- script.js
+++ script.js
@@ -1,34 +1,54 @@
-const WPM = 500;
+let WPM = 400;
+let currentIndex = 0;
 
-function start() {
+const start = () => {
     let inputBox = document.getElementById("input");
     let data = inputBox.value.split(" ");
-    animate(data);
+    data.unshift("{WPM_" + WPM + "}");
+    for (let i = 1; i <= 3; i++) {
+        data.unshift(i.toString());
+    }
+    data.unshift("{WPM_1}");
+    console.log(data);
+    process(data);
+   // animate(data);
 }
 
-function animate(data) {
-    let animation = document.getElementById("reader");
+let process = (data) => setInterval(() => {
+    console.log(data[currentIndex]);
 
-    let countdown;
-    let count = 3;
-    animation.innerHTML = count;
-    countdown = setInterval(function() {
-        count--;
-        if (count == 0) {
-            clearInterval(countdown);
-            let index = 0;
-            animation.innerHTML = data[index];
-            let inputAnimation = setInterval(function() {
-                index++;
-                if (index == data.length) {
-                    clearInterval(inputAnimation);
-                } else {
-                    animation.innerHTML = data[index];
-                }
-            }, 60000/WPM)
-        } else {
-            animation.innerHTML = count;
-        }
-    }, 1000);
 
-}
+    
+    
+
+    currentIndex++;
+}, 60000/WPM);
+
+// function animate(data) {
+//     let animation = document.getElementById("reader");
+
+//     let countdown;
+//     let count = 3;
+//     animation.innerHTML = count;
+//     countdown = setInterval(function() {
+//         count--;
+//         if (count == 0) {
+//             clearInterval(countdown);
+//             let index = 0;
+//             animation.innerHTML = data[index];
+//             let inputAnimation = setInterval(function() {
+//                 index++;
+//                 if (index == data.length) {
+//                     clearInterval(inputAnimation);
+//                 } else {
+//                     animation.innerHTML = data[index];
+//                 }
+//             }, 60000/WPM)
+//         } else {
+//             animation.innerHTML = count;
+//         }
+//     }, 1000);
+
+// }
+
+start();
